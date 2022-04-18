db7d2a369f947c83984537b2dd9ab1f7c32093e2
zampierre <pedro.zampieri01@gmail.com>
2022-04-17 23:40:21 -0300

    Conversor de idade de humano para de cachorro

---
+++ dogYears.js
@@ -0,0 +1,10 @@
+let myAge = 20
+let earlyYears = 2
+earlyYears *= 10.5
+let laterYears = myAge - 2
+laterYears *= 4
+let myAgeInDogYears = earlyYears + laterYears
+const myName = 'Pedro Zampieri'.toLowerCase()
+console.log(
+  `My name is ${myName}, I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`
+)
